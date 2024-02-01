3ec195578c2b4b1aa559832a62a979f61ea3969e
Marigela Bhavana <marigelabhavana2000@gmail.com>
2024-02-01 19:58:54 +0530

    Add project files.

---
+++ Feb1Assessment/Controllers/CustomerController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Feb1Assessment.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            List<string> list = new List<string>()
+            {
+            "David",
+            "Sam",
+            "Mike",
+            "Madhav",
+            "Kate"
+            };
+            ViewBag.NameList = list;
+            return View();
+        }
+        public ActionResult ReDirectDemo() 
+        {
+            TempData["WelcomeMessage"] = "Welcome to Customer Page";
+            return RedirectToAction("DisplayMessage");
+        }
+        public ActionResult DisplayMessage() 
+        {
+            string welcomeMessage = TempData["WelcomeMessage"] as string;
+            TempData.Remove("WelcomeMessage");
+            ViewBag.WelcomeMessage = welcomeMessage;
+            return View();
+        }
+    }
+}
---
+++ Feb1Assessment/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Feb1Assessment.MvcApplication" Language="C#" %>
